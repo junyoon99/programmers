2c482e5bfea5b27a9c4517affb0391e782f7e48e
junyoon99 <junyoon99@naver.com>
2024-09-12 02:03:18 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = -1;
+    int a = sqrt(n);
+    if (!(sqrt(n) - a)) return pow(sqrt(n) + 1, 2);
+
+    return answer;
+}
