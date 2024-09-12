b5d4fab27d52636c3549c58d912974de63a94646
junyoon99 <junyoon99@naver.com>
2024-09-13 02:30:32 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    
+    for(int i{}; i < absolutes.size(); i++)
+    {
+        answer += absolutes[i] * (signs[i] ? 1 : -1);
+    }
+    
+    return answer;
+}
