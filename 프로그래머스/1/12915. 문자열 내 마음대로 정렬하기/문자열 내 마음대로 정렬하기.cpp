4b34885e78deee6455f5dae06cc2c75e3dea6d09
junyoon99 <junyoon99@naver.com>
2024-10-09 07:37:20 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.06 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    
+    sort(strings.begin(), strings.end(),[n](string a, string b)->bool{
+        if(a[n] == b[n])
+        {
+           int s{};
+            while(a[s] == b[s])
+            {
+                s++;
+            }
+            return a[s] < b[s];
+        }
+        return a[n] < b[n];
+    });
+    
+    return strings;
+}
